750d0ef7730a21d65471c34588e373582f1b08df
highonweb <mothish07122001@outlook.com>
2020-09-16 13:09:50 +0530

    completed

---
+++ index.js
@@ -0,0 +1,50 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+const memebox = document.querySelector('.memes')
+canvas.width = window.innerWidth * 0.9
+canvas.height = window.innerHeight * 0.9
+let memes
+const getmeme = async () => {
+  memes = await fetch('https://api.imgflip.com/get_memes')
+  memes = await memes.json()
+  memes = memes.data.memes
+  for (i in memes) {
+    const img = document.createElement('img')
+    img.src = memes[i].url
+    img.style.width = '24vw'
+    img.setAttribute('onclick', `select(${i},this)`)
+    img.className = 'meme'
+    memebox.appendChild(img)
+  }
+}
+const font = ()=>{
+    document.querySelector('.fontsize').style.fontSize= document.getElementById('font').value + 'px'
+}
+
+getmeme()
+const select = (id, img) => {
+
+
+  ctx.drawImage(img, 0, 0, memes[id].width / memes[id].height * canvas.height, canvas.height)
+}
+const newtext = () => {
+  canvas.addEventListener('click', matext)
+
+  canvas.style.cursor = "url('./text.png'),auto"
+  ctx.save();
+  
+}
+const matext = (e) => {
+  var textie = prompt('Please enter the text', 'Doodles')
+  ctx.font = document.getElementById('font').value + 'px Times New Roman'
+  ctx.strokeStyle = document.getElementById('stroke-color').value
+  ctx.lineWidth = 2
+  ctx.textAlign = 'left'
+
+  ctx.lineJoin = 'miter' // Experiment with "bevel" & "round" for the effect you want!
+  ctx.miterLimit = 2
+  ctx.strokeText(textie, e.clientX + 40, e.clientY + 40)
+  ctx.fillStyle = document.getElementById('fill-color').value
+  ctx.fillText(textie, e.clientX + 40, e.clientY + 40)
+  ctx.restore()
+}
